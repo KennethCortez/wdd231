2e7db94667e8d5dc58094cdec5547def4285f530
KennethCortez <kennethleocortez@gmail.com>
2025-07-06 23:19:08 -0600

    v1.2

--- scripts/home.js
+++ scripts/home.js
@@ -21,19 +21,130 @@ document.addEventListener("DOMContentLoaded", function () {
         lastModifiedElement.innerHTML = `Last Modified: ${formattedDate}`;
     }
 
-// ======= Hamburger menu and blur effect =======
+// ======= Hamburger menu =======
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const background = document.querySelector("main");
 
-if (hamburger && navMenu && background) {
+if (hamburger && navMenu) {
     hamburger.addEventListener("click", function () {
     navMenu.classList.toggle("open");
 
     const isOpen = navMenu.classList.contains("open");
     hamburger.setAttribute("aria-expanded", isOpen);
 
-    background.classList.toggle("blur", isOpen);
     });
     }
+});
+
+// ======= Course List Filtering =======
+const courses = [
+    {
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    }
+]
+
+const courseList = document.getElementById('courseList');
+const allBtn = document.getElementById('allBtn');
+const wddBtn = document.getElementById('wddBtn');
+const cseBtn = document.getElementById('cseBtn');
+
+function renderCourses(list) {
+    courseList.innerHTML = '';
+
+    list.forEach(course => {
+    const li = document.createElement('li');
+    li.classList.add('course-item');
+    li.classList.add(course.completed ? 'completed' : 'incomplete');
+    li.textContent = course.title;
+    courseList.appendChild(li);
+    });
+}
+
+renderCourses(courses);
+
+// filter event listeners
+allBtn.addEventListener('click', () => renderCourses(courses));
+
+wddBtn.addEventListener('click', () => {
+    const wddCourses = courses.filter(c => c.subject === 'WDD');
+    renderCourses(wddCourses);
+});
+
+cseBtn.addEventListener('click', () => {
+    const cseCourses = courses.filter(c => c.subject === 'CSE');
+    renderCourses(cseCourses);
 });
